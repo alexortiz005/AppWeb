2d28ae9ffa8e3638eb08d83c2364a43ac33ac9ef
Felipevd <felipevillarrealdaza@outlook.com>
2016-02-16 13:34:18 +0100

    Update mapa.js

--- grails-app/assets/javascripts/mapa.js
+++ grails-app/assets/javascripts/mapa.js
@@ -7,22 +7,22 @@ var map;
    			zoom: 9,
 		}
 		map = new google.maps.Map(document.getElementById('mapa'),options);
+		var bikeLayer = new google.maps.BicyclingLayer();
+  		bikeLayer.setMap(map);
 	}
-	var bikeLayer = new google.maps.BicyclingLayer();
-  	bikeLayer.setMap(map);
 	function addMarkerToMap(nombre, lat, lon, tipo){
-    	var infowindow = new google.maps.InfoWindow();
-    	var myLatLng = new google.maps.LatLng(lat, lon);
-    	var marker = new google.maps.Marker({
-    	    position: myLatLng,
-    	    map: map,
-    	    icon: '../assets/'+tipo+'_icon.png',
-    	});
-    }
-    function test(puntosJSON){
-    	initMap();
-    	for (var i=0; i < puntosJSON.length; i++) {
-    		var punto = puntosJSON[i];
+    		var infowindow = new google.maps.InfoWindow();
+    		var myLatLng = new google.maps.LatLng(lat, lon);
+    		var marker = new google.maps.Marker({
+    		    position: myLatLng,
+   		    map: map,
+	 	    icon: '../assets/'+tipo+'_icon.png',
+    		});
+	  }
+	function test(puntosJSON){
+    		initMap();
+    		for (var i=0; i < puntosJSON.length; i++) {
+    			var punto = puntosJSON[i];
 			addMarkerToMap(punto.nombre,parseInt(punto.lat),parseInt(punto.lon),"parking");
 		}
-    }
+	}
